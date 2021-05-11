085ace8355f5c8673e6815bc5981262e753928dc
Gabriel Luiz Nunes de Queiroz <gabriel.queiroz@polichat.com.br>
2021-05-11 17:07:23 -0300

    end stats

--- stats.js
+++ stats.js
@@ -1,3 +1,27 @@
 const os = require('os');
 
-console.log(os);
+/* Criando um objeto de maneira tradicional 
+const freemem = os.freemem;
+const totalmem = os.totalmem;
+*/
+
+// Função que executa a cada tempo determinado (ms)
+setInterval( () => {
+  // Desestruturação de um objeto.
+  const { freemem, totalmem } = os;
+  
+  const total = parseInt(totalmem() / 1024 / 1024);
+  const mem = parseInt(freemem() / 1024 / 1024);
+  const percents = parseInt((mem / total) * 100);
+
+  const stats = {
+    free: `${mem} MB`,
+    total: `${total} MB`,
+    usage: `${percents} %`,
+  }
+  
+  console.clear();
+  console.log('======= PC STATS ======');
+  console.table(stats);
+}, 1000);
+
